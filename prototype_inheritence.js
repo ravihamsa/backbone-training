1853f4ff2edcee9155b9a0fd973f3266fb476023
Ravi Kumar Hamsa <ravikumar.anantha@yahoo.in>
2013-10-09 18:39:05 +0530

    Update prototype_inheritence.js

--- prototype_inheritence.js
+++ prototype_inheritence.js
@@ -2,12 +2,40 @@
 
 var Vehicle = function(){
   this.run = function(){
-    console.log('I can run');
+    return 'I can run';
   }
 }
 
 var TwoWheeler = function(){
    this.wheels = 2;
+   this.getWheelCount = function(){
+     return this.wheels;
+   }
 }
 
 TwoWheeler.prototype = new Vehicle(); // notice "new" keyword here
+
+
+var twoWheeler = new TwoWheeler();
+console.log(twoWheeler.getWheelCount()) //2
+console.log(twoWheeler.run()) //I can run
+
+
+//Overrding
+
+var BullockCart = function(){
+  this.run = function(){
+    throw new Error('I can not run');
+  }
+  
+  this.pull = function(){
+    return 'I can pull'
+  }
+}
+
+TwoWheeler.prototype = new Vehicle();
+
+var bullockCart = new BullockCart();
+console.log(bullockCart.pull());
+console.log(bullockCart.run())
+
